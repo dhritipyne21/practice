8b88b53f5bb44f9173eccf3d873ebf188d207c20
Dhriti Pyne <dhriti.eduapp@gmail.com>
2022-04-01 21:11:02 +0530

    JSX Basics

---
+++ src/components/JSXExplain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const JSXExplain = () => {
+  return (
+    // <div>JSXExplain</div>
+    React.createElement('div', {}, 'JSXExplain')//tag, props {key:"ch"}, ''(tag,props,child)
+  )
+}
+
+export default JSXExplain
